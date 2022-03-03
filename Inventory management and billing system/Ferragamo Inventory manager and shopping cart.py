8ae8688da13fdada974c990503827ebc3d45fc66
KeaganSuah <100312748+KeaganSuah@users.noreply.github.com>
2022-03-03 20:51:14 +0800

    Update Ferragamo Inventory manager and shopping cart.py
    Functions and variables have been changed for better readability. An additional function was added that can prompt users that they have typed an invalid command into the input.

--- Inventory management and billing system/Ferragamo Inventory manager and shopping cart.py
+++ Inventory management and billing system/Ferragamo Inventory manager and shopping cart.py
@@ -1,8 +1,3 @@
-#Making a Ferragamo shopping list
-# 1. display, and append the inventory count ( display item together and individually)
-# 2. calcuate the billing item
-# 3. display the discounted percentage of the items
-# 4. able to record the total sales generated
 import os
 import csv
 
@@ -17,27 +12,28 @@ for root, dirs, files in os.walk(dir_path):
 f = open(filepath,'r')
 readr = csv.reader(f)
 #The data in the csv file will be transfered into python as a dictionary item
-itemDict = {}
+inventory_dict = {}
 
 for row in readr:
-    itemDict[row[0]]= [row[1],row[2],row[3],row[4],row[5]]
+    inventory_dict[row[0]]= [row[1],row[2],row[3],row[4],row[5]]
     # for this, the main key of the dictionary will be the model number, and he values will be stored in a list of model name, price, quantity, colour, and size
 
-def showitems(item, feature):
+
+def show_features(item, feature):
     # This will be the main function for other function that requires a display of items
     return('Model Number : ' + str(item) + ' | name : ' + str(feature[0])+ ' | colour : ' + str(feature[3]) + '| size : ' +str(feature[4]) + '| Qty : ' + str(feature[2]))  
 
-def displayItems(list):
+def billing_list(list):
     # This function is used to display item in a format of model name, colour and size only. This remove other features that customer are not interested in
     product_type = '6.5'
     # For this function to work, I've decided to print all the models but only the size 4 items, as all the shoe has a size 6.5 in common, this will only show 1 size for each model, preventing any duplicates in the list
     print('Item listing \n ------------------')
     for item, feature in list.items():
         if product_type == feature[4]:
-            print(showitems(item, feature).replace('Model Number : ' + str(item),'').replace('| size : ' +str(feature[4]) + '| Qty : ' + str(feature[2]),'') + ' | price : $' + str(feature[1]))
+            print(show_features(item, feature).replace('Model Number : ' + str(item),'').replace('| size : ' +str(feature[4]) + '| Qty : ' + str(feature[2]),'') + ' | price : $' + str(feature[1]))
     print('------------------')            
     
-def displayitem_no(list):
+def display_model_number(list):
     #function display items by its model number, this list will only have 1 item showing
     while True:
         product_type = str(input('enter the model number you like to view: '))
@@ -46,9 +42,10 @@ def displayitem_no(list):
         else:
             for item, feature in list.items():
                 if product_type == item:
-                    print(showitems(item,feature).replace('Model Number : ' + str(item), ''))        
+                    print(show_features(item,feature).replace('Model Number : ' + str(item), ''))  
+               
     
-def displayname(list):
+def display_model_name(list):
     #This display item by its name, usually, this will be the most common way of navigating through the inventories
     while True:
         product_type = str(input('enter the name you like to view: '))
@@ -57,10 +54,10 @@ def displayname(list):
         else:
             for item, feature in list.items():
                 if product_type == feature[0]:
-                    print(showitems(item, feature).replace(' | name : ' + str(feature[0]), ''))
+                    print(show_features(item, feature).replace(' | name : ' + str(feature[0]), ''))
                     
                        
-def displaysize(list):
+def display_by_size(list):
     # This display all the models by the size the user type in. This function is useful for consumers that only have a special size that they can wear. Hence searching by size, allow employees to pull out all the models of the consumers size for the consumers
     while True:
         product_type = str(input('enter the size you like to view: '))
@@ -69,9 +66,9 @@ def displaysize(list):
         else:
             for item, feature in list.items():
                 if product_type == feature[4]:
-                    print(showitems(item, feature).replace('| size : ' +str(feature[4]), ''))   
+                    print(show_features(item, feature).replace('| size : ' +str(feature[4]), ''))   
                     
-def displaycolour(list):
+def display_by_colour(list):
     # Consumers have different taste and prefernce, some would only like to buy shoes of their favourite colour. With this function, it allows user to list all the inventories that's the same colour as what the user has inputted
     while True:
         product_type = str(input('enter the colour you like to view: '))
@@ -80,63 +77,67 @@ def displaycolour(list):
         else:
             for item, feature in list.items():
                 if product_type == feature[3]:
-                    print(showitems(item, feature).replace(' | colour : ' + str(feature[3]), ''))   
+                    print(show_features(item, feature).replace(' | colour : ' + str(feature[3]), ''))   
                     
-def displaystock(list):
+def inventory_display(list):
     #This is the main function for displaying all the stocks, if user were to choose a specific method of listing the inentories, other function will operate.
     viewing_type = str(input('Choose what would you like to view (model number / name / size / colour / or all): '))
     if viewing_type == 'all':
         print('Stock Count list \n ------------------')
         for item, feature in list.items():
-            print(showitems(item, feature))
+            print(show_features(item, feature))
         print('-----------------------')
     elif viewing_type == 'model number':
-        displayitem_no(list)
+        display_model_number(list)
     elif viewing_type == 'name':
-        displayname(list)
+        display_model_name(list)
     elif viewing_type == 'size':
-        displaysize(list)
+        display_by_size(list)
     elif viewing_type == 'colour':
-        displaycolour(list)
+        display_by_colour(list)
+    else:
+        print(viewing_type + ' is a invalid command!')
         
 def billing(list):
     # This is the billing function that charges the consumers for what they have purchased
     total_bill = 0
     while True:
-        displayItems(itemDict)
+        billing_list(inventory_dict)
         model_type = str(input('model name: '))
         colour_type = str(input('colour: '))
         if model_type == 'end':
             break
         else:
-            for item, feature in itemDict.items():
+            for item, feature in inventory_dict.items():
                 if model_type == feature[0] and colour_type == feature[3]:
-                    print(showitems(item, feature).replace(' | name : ' + str(feature[0])+ ' | colour : ' + str(feature[3]),'') + ' | price : $' + str(feature[1])) 
-            user_item = str(input('Please key in the model number: '))
+                    print(show_features(item, feature).replace(' | name : ' + str(feature[0])+ ' | colour : ' + str(feature[3]),'') + ' | price : $' + str(feature[1])) 
+            user_keyed_item = str(input('Please key in the model number: '))
             # Most stores will usually scan the item model when processing a transaction, as the model number includes the model type, the size and the colour
-            if user_item in list:
+            if user_keyed_item in list:
                 try:
-                    no_item = int(input('Enter the number of item purchased: '))
+                    number_item = int(input('Enter the number of item purchased: '))
                 except ValueError:
                     print('Please enter a number')
                 else:
-                    quantity = list[user_item][2]
+                    quantity = list[user_keyed_item][2]
                     quantity = int(quantity)
                     # This will remove update the quantity left automatically, so when a consumer purhcase X amount of item, it will minus X amount from the inventory list
-                    if no_item > int(quantity):
+                    if number_item > int(quantity):
                         print('Sorry, insufficient amount')
                     else:
-                        quantity -= no_item
-                        chosen_item = list.get(user_item)
-                        total_bill += float(chosen_item[1])*no_item
+                        quantity -= number_item
+                        chosen_item = list.get(user_keyed_item)
+                        total_bill += float(chosen_item[1])*number_item
                         # It calcualates and total up the inventory purchased and it will be shown at the end
-                        list[user_item][2] = str(quantity)
+                        list[user_keyed_item][2] = str(quantity)
                         proceed = str(input('Would you like to process another transaction? (yes/no) : '))
                         if proceed == 'no':
                             print("--------------- \nTotal bill is : ${}\n--------------".format(round(total_bill,3)))
                             break
                         else: 
-                            print('Item not found')                              
+                            print('Item not found')
+            else:
+                print('The model number ' + user_keyed_item + ' cannot be found in the list')
         
 while True:
     # The main operation of this program, it is the start and the end of the program. 
@@ -147,7 +148,8 @@ while True:
         break
     else:
         if activity == 'view':
-            displaystock(itemDict)
+            inventory_display(inventory_dict)
         elif activity == 'bill':
-            billing(itemDict)
-                
+            billing(inventory_dict)
+        else:
+            print(activity + ' is a invalid command!')                
